c5b10a1a4c2377432b438a58b38d923fe29bc3fa
kykkyn2 <kykkyn2@gmail.com>
2014-04-21 18:48:38 +0900

    Update casper.js

--- casper.js
+++ casper.js
@@ -2,8 +2,8 @@ var casper = require('casper').create();
 
 casper.start('https://www.facebook.com/' , function (){
 	 this.fill('form#login_form', {
-	        'email': 'kykkyn2@gmail.com',
-	        'pass': 'qkrwjd1'
+	        'email': 'xxxxxxx@gmail.com',
+	        'pass': 'xxxxxxx'
 	 }, true);
 });
 
